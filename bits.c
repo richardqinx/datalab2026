e5c1302ea129c0a0cf8ae704c4f3ad938cb34489
Richard Qin <richard.qin.001@gmail.com>
2026-09-22 00:48:10 +0800

    floatPower2

--- bits.c
+++ bits.c
@@ -311,5 +311,14 @@ int float64_f2i(unsigned uf1, unsigned uf2) {
  *   Difficulty: 4
  */
 unsigned floatPower2(int x) {
-    return 2;
+    if (x < -149) {
+        return 0;
+    }
+    if (x < -126) {
+        return 1 << (x + 149);
+    }
+    if (x <= 127) {
+        return (x + 127) << 23;
+    }
+    return 0x7F800000;
 }
